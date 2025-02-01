e51fd921ad20c0c6a962474eec0143fd911a14ef
zoro <kr.ankit253@gmail.com>
2025-02-01 20:47:00 +0530

    add config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    distDir: 'dist',
+    output: 'export',
     images: {
-        unoptimized: true,
-    }
-}
-
+      unoptimized: true,
+    },
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  }
+  
 module.exports = nextConfig
